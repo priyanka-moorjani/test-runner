5155b7a480946bd3a3f91646fa1d95ef56a65d9e
dandhimdis <dan.dhimdis@chromeriver.com>
2018-03-09 12:00:23 -0800

    add second test pr

--- test-web-hook.js
+++ test-web-hook.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const util = require('util');
+const http = require('http');
+const request = require('request');
 
 
 // Constants
@@ -17,11 +19,49 @@ app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
 
-  console.log('request comming in');
-  console.log(req.body);
-  res.send('got it');
+  var payload = res.body.payload;
+  var body = JSON.parse(body);
+  var pr = body.pull_request;
+  var statusURl = body.statuses_url;
 
-});
+  console.log('statusURL: ' + statusURl);
+  // var options =. {
+  //   host: '',
+  //   path: '',
+  //   port: ''
+  //   method: 'post'
+  // }
+  var options = {
+    uri: statusURl,
+    method: 'POST',
+    // body: '',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  var statusRequest = {
+    state: "success",
+    target_url: "",
+    description: "test passed"
+    context: "continuous-integration"
+  };
+
+  options.body = JSON.stringify(statusRequest);
+
+  request.post(options, (err, response) => {
+    if (err) {
+      console.log('error');
+      console.log(err);
+    } else {
+      console.log('success');
+      console.log(response);
+    }
+
+    res.send('got it');
+  });
+
+ });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
