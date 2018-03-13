3e6b1567bf64587ae9ee7cb065537bea8258cda5
dandhimdis <dan.dhimdis@chromeriver.com>
2018-03-13 14:36:06 -0700

    Initial commit

---
+++ test-runner-image/server.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require("body-parser");
+const git = require('nodegit');
+const path = require('path');
+const _get = require('lodash/get');
+const local = path.join.bind(path, __dirname);
+const { exec } = require('child_process');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const sshPublicKeyPath = local("./id_rsa.pub");
+const sshPrivateKeyPath = local("./id_rsa");
+
+// App
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
+
+app.get('/healthCheck', (res) => {
+ console.log('Health Check') ;
+ res.send('Ok');
+});
+
+app.post('/', (req, res) => {
+
+  console.log(req.body);
+  res.setHeader('Content-Type', 'application/json');
+  var branch = _get(req, 'body.branch');
+  var repository;
+  var debug = 0;
+
+  if(!branch) {
+    errorHandler('branch not specified');
+    return;
+  }
+  // Open repo and pull branch
+  git.Repository.open('./mercury')
+  
+  .then((repo) => {
+    repository = repo;
+
+    return repository.fetch('origin', {
+
+      callbacks: {
+
+        certificateCheck: function() {
+            return 1;
+        },
+        credentials: function(url, userName) {
+          // avoid infinite loop when authentication agent is not loaded
+          if (debug++ > 10) throw 'Git authorization failed';
+          // return git.Cred.sshKeyFromAgent(userName);
+          // return git.Cred.defaultNew();
+          return git.Cred.sshKeyNew(
+              userName,
+              sshPublicKeyPath,
+              sshPrivateKeyPath,
+              "");
+        }
+      }
+
+    }, true);
+
+  })
+  .then(() => {
+    return repository.getBranch('refs/remotes/origin/' + branch);
+  })
+  // check out request branch
+  .then((reference) => {
+    return repository.checkoutRef(reference);
+  //install dependencies and run unit test
+  }).then(() => {
+
+    exec('npm install && bower install --allow-root', { cwd: './mercury', maxBuffer: 1024 * 5000 }, (err, stdout, stderr) => {
+
+      if (err) {
+        errorHandler(err);
+        return;
+      }
+      
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+
+
+      exec('npm run karma:phantomJS', { cwd: './mercury', maxBuffer: 1024 * 5000 }, (err, stdout, stderr) => {
+
+        if (err) {
+          console.log(err);
+          console.log('fail');
+          res.send(JSON.stringify({ status: 'fail' }));
+          return;
+        }
+
+        console.log('pass');
+        res.send(JSON.stringify({ status: 'pass' }));
+      });
+
+    });
+
+  }, errorHandler);
+
+
+  function errorHandler (err) {
+    console.log(err);
+    res.send(JSON.stringify({ status: 'error', error: err }));
+  }
+
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
---
+++ test-runner-lambda/index.js
@@ -0,0 +1,94 @@
+'use strict';
+const request = require('request-promise');
+const _get = require('lodash/get');
+const _includes = require('lodash/includes');
+const GIT_TOKEN = process.env.Token;
+const TEST_RUNNER_URL = process.env.Test_Runner_Url;
+const actions = ['reopened', 'opened', 'synchronize'];
+
+
+const STATUS_SUCCESS = 'success';
+const STATUS_PENDING = 'pending';
+const STATUS_FAIL = 'failure';
+const STATUS_ERROR = 'error';
+
+
+var headers =  {
+    'Content-Type': 'application/json',
+    'user-agent': 'node.js',
+    'Authorization': 'token ' + GIT_TOKEN
+};
+
+module.exports.handler = (event, context, callback) => {
+
+    var response = {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: { },
+        body: '{}'
+    };
+
+    console.log(GIT_TOKEN);
+    console.log(TEST_RUNNER_URL);
+    callback(null, response);
+
+    var body = event.body? JSON.parse(event.body): {};
+    var statusURl = _get(body, 'pull_request.statuses_url');
+    var branch = _get(body, 'pull_request.head.ref');
+    var commit = _get(body, 'pull_request.head.sha');
+    var action = _get(body, 'action');
+
+    var statusRequest = {
+        state: STATUS_PENDING,
+        description: STATUS_PENDING,
+        context: "mercury unit test"
+    };
+
+    if (!statusURl || !branch || !_includes(actions, action)) {
+        //exit function
+        return;
+    }
+
+    //update status to pending
+    request.post(statusURl, { json: statusRequest, headers: headers })
+     //call unit test runner service
+    .then((body) => {
+
+        // var state = _get(body, 'state');
+        // if(state !== 'pending') return;
+
+        return request.post(TEST_RUNNER_URL, { json: {
+            branch: branch,
+            commit: commit
+        }});
+
+    })
+     //update github status from unit test service response
+    .then((body) => {
+
+        var status = _get(body, 'status');
+
+        if(status === 'pass') {
+            statusRequest.state = STATUS_SUCCESS;
+            statusRequest.description = 'Unit test succeeded';
+        } else if (status === 'fail') {
+            statusRequest.state = STATUS_FAIL;
+            statusRequest.description = 'Unit test failed';
+        } else if (status === 'error') {
+            statusRequest.state = STATUS_ERROR;
+            statusRequest.description = "Error running unit test";
+        }
+
+        if(statusRequest.state === STATUS_PENDING) return;
+
+        return request.post(statusURl, { json: statusRequest, headers: headers });
+    })
+    .catch((err) => {
+        console.log('Request ERROR');
+        console.log(body);
+        console.log(err);
+    });
+
+};
+
+
